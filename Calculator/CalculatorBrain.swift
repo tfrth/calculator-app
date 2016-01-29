89c91a214a7f384d1d382234dce7194e0eeca2d5
Ty Frith <tyfrith@gmail.com>
2016-01-29 14:23:22 -0800

    added CalculatorBrain model

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,89 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Tyler Frith on 1/27/16.
+//  Copyright © 2016 tfrth. All rights reserved.
+//
+
+//the "model" in MVC
+
+import Foundation
+
+class CalculatorBrain {
+    
+    private enum Op: CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()          //creates a dictionary
+    
+    init() {                                        //initializer
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+        
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops    //makes copy of ops to be mutable array to work with
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation (_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluaiton = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluaiton.result {
+                        return (operation(operand1, operand2), op2Evaluaiton.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)   //default return nil if ops IS empty
+    }
+    
+    func evaluate() -> Double? {  //must be an optional b/c nil may need to be returned (ie someone just enters in + but no operands)
+        let (result, _) = evaluate(opStack)
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {   //function to look up values in dictionary
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
+
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!  //property syntax
     
-    var userIsInTheMiddleOfTypingANumber = false
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    var brain = CalculatorBrain()
+    
+    //creates instance of CalculatorBrain "
+    //"green arrow" that goes from the controller to the model
 
     @IBAction func appendDigit(sender: UIButton) {
         
@@ -27,63 +32,47 @@ class ViewController: UIViewController {
        
     }
     
-  
+//    @IBOutlet weak var history: UILabel!  //not sure how to implement history label 
+    
+    @IBAction func clear(sender: UIButton) {
+        displayValue = 0;
+//        operandStack.removeAll();
+//        print(operandStack)
+    }
+    
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
-        switch operation {
-            case "×": performOperation { $0 * $1 }
-            case "÷": performOperation { $1 / $0 }
-            case "+": performOperation { $0 + $1 }
-            case "−": performOperation { $1 - $0 }
-            default: break
-
-            
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            }
+        } else {
+            displayValue = 0
         }
     }
     
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-        }
-    }
-    
-    //performOperation function is allowing us to condense operation into one function since all operators are very similar in that they take two doubles and return another double.
-    
-//    func multiply(op1: Double, op2: Double) -> Double {  //multiply bit
-//        return op1 * op2
-//    }
-//    
-//    func divide(op1: Double, op2: Double) -> Double {  //divide bit
-//        return op1 / op2
-//    }
-//    
-//    func add(op1: Double, op2: Double) -> Double {  //add bit
-//        return op1 + op2
-//    }
-//    
-//    func subtract(op1: Double, op2: Double) -> Double {  //subtract bit
-//        return op1 - op2
-//    }
-    
-    var operandStack = Array<Double>() //var can infer type from value i.e Array<Double>
+   
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        print("operandStack = \(operandStack)")
-        
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double {
         get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue //
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
             display.text = "\(newValue)"
             userIsInTheMiddleOfTypingANumber = false
         }
     }
+    
+    
 }
 
